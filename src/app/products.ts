3019e2410f8cc28bb8144cebf3a6afc48c516414
DESKTOP-FBH0P1R\34605 <camposvmiguelangel@gmail.com>
2021-06-06 16:48:05 +0200

    img src

--- src/app/products.ts
+++ src/app/products.ts
@@ -13,22 +13,22 @@ export const products = [
   {
     id: 1,
     name: 'Cubot Note 20',
-    price: 87.95,
+    price: 87.98,
     excerpt: 'The NOTE 20 free phone has a large 6.5-inch raindrop screen with rounded corners, offers a better visual experience.',
     description1:'【6.5-inch large screen NOTE The NOTE 20 free mobile has a large 6.5-inch water drop screen with rounded corners, offers a better visual experience, you can see a full screen, be it videos, games or anything else, it is your Best partner. 【SONY Quad Camera】 The cheap and good NOTE 20 mobile with quad rear camera designed in a round shape, looks fashionable.',
     description2:'【 With SONY IMX486 12.0MP main camera capture the beauties of your life clearly, with the Sony IMX350 20MP sensor capture portraits like a film director with movie mode. 8MP front camera enhances images with AI, record every point of your beauty. 3GB + 64GB + 4200mAh Battery】',
     description3:'The 4G smartphone has a large 4200mAh battery, enough for your everyday use, spend all day using it and always with power. 64GB ROM capacity allows you to take multiple photos and videos, install more applications and games, if you still do not have enough memory, expand it with a tf card up to 128GB.',
-    img: '../assets/img/mobile-models/Cubot.webp'
+    img: 'assets/img/mobile-models/Cubot.webp'
   },
   {
     id: 2,
     name: 'Dodge Pro2021',
-    price: 199.70,
+    price: 199,
     excerpt:'DOOGEE S59 Pro comes with a huge 10050mAh battery inside and can handle intense gaming for long hours.',
     description1: 'DOOGEE S59 Pro comes with a huge 10050mAh battery inside and can handle intense gaming for long hours. Enjoy fast charging with 24W cables anytime, anywhere. Because this phone can provide up to 700 hours of standby time, you can easily get rid of the battery anxiety problem.',
     description2:'With up to 4GB RAM and 128GB of internal storage, it reveals the power performance of the DOOGEE S59 Pro, giving you a lot more space than other smartphones in the same range.',
     description3:'Android 10.0 has been completely modified to improve your experience of speed and power. With an octa-core processor, you can multitask at high speed, providing smooth response and ultra-fast playback experience.',
-    img: '../assets/img/mobile-models/Doodge Pro2021.webp'
+    img: 'assets/img/mobile-models/Doodge Pro2021.webp'
   },
   {
     id: 3,
@@ -38,17 +38,17 @@ export const products = [
     description1: '6.3 "OLED Dewdrop Display: With a 90.17% screen ratio and ultra-clear 2400 x 1080 resolution, the 6.3" OLED dewdrop display provides a remarkable visual experience and cinematic color gamut.',
     description2:'On-screen fingerprint: With advanced on-screen fingerprint technology, the unlock speed and success rate of HUAWEI P Smart S are greatly improved.',
     description3:'48MP Triple AI Camera: 48MP ultra-clear main lens, 8MP ultra-wide angle lens and 2MP depth lens, help you explore mysterious nature.',
-    img: '../assets/img/mobile-models/Huawei P Smart S.webp'
+    img: 'assets/img/mobile-models/Huawei P Smart S.webp'
   },
   {
     id: 4,
     name: 'Motorola E7 Plus',
-    price: 130.50,
+    price: 130.55,
     excerpt:'48 MP camera system. * Capture sharper and brighter photos even at night and add a beautiful blur effect to your portraits.',
     description1: '48 MP camera system. * Capture sharper and brighter photos even at night and add a beautiful blur effect to your portraits. Battery life of more than two days. 1 Keep having fun without having to charge your phone thanks to a 5000 mAh battery full of power.',
     description2:'50% faster AI-enabled performance # Take your best photos and feel the power of an octa-core Qualcomm Snapdragon processor and 4GB of memory. 6.5 "HD + Max Vision screen.',
     description3:'Get carried away playing games, watching movies and talking with your friends on an ultra-wide screen. 64 GB of internal storage.',
-    img: '../assets/img/mobile-models/Motorola E7 Plus.webp'
+    img: 'assets/img/mobile-models/Motorola E7 Plus.webp'
   },
   {
     id: 5,
@@ -58,17 +58,17 @@ export const products = [
     description1:'Redmi 9; The incredible Redmi series offers you the best experience, continuing the legacy of its predecessors. Improved imaging system, 13 MP quad camera with AI and 8 MP front selfie camera.',
     description2:'Redmi 9 features a 6.53 "HD + DotDisplay screen with TÜV Rheinland low blue light certification. MediaTek Helio G80 processor, high-performance 8-core processor.',
     description3:' Upgraded 5020mAh (typ) battery, you can enjoy long-lasting power; Together With 18W fast charging, you can enjoy your device in no time.',
-    img: '../assets/img/mobile-models/REdmi note 10.webp'
+    img: 'assets/img/mobile-models/REdmi note 10.webp'
   },
   {
     id: 6,
     name: 'Samsung Galaxy A21s',
-    price: 177.50,
+    price: 177.75,
     excerpt: 'Mobile phone camera: 48 MP main camera, 2 MP focal camera, 8 MP ultra wide angle, 2 MP macro camera and 13 MP nominal front camera',
     description1:'Mobile phone camera: 48 MP main camera, 2 MP focal camera, 8 MP ultra wide angle, 2 MP macro camera and 13 MP nominal front camera. Smartphone display: 6.5-inch TFT LCD Infinity-O display, 720 x 1600 pixel resolution, 409 dpi density and 16 million colors.',
     description2:'Smartphone memory: microSD card up to 512 GB; an expandable memory for your photos, your music and much more. ',
     description3:'Mobile phone battery: 5000 mAh high-performance lithium-ion battery with 15 W fast charge. Smartphone with Android V10.0 operating system.',
-    img: '../assets/img/mobile-models/Samsung Galaxy A21s.webp'
+    img: 'assets/img/mobile-models/Samsung Galaxy A21s.webp'
   },
   {
     id: 7,
@@ -78,7 +78,7 @@ export const products = [
     description1: 'Ulefone Note 10 Mobile Phone runs the latest Android 11 Go Edition operating system out of the box, allows you to easily check important messages and manage conversations, connected devices, privacy settings and other content in a more convenient way.',
     description2:'[High-performance CPU + 4G Dual SIM] UlefoneNote10 uses SC9863A chip, eight-core processor, RAM2GB + 32GB ROM (the maximum can be expanded to 128GB). The operational performance is stable, which can effectively reduce power consumption and provide enough buffer for operation and storage space. It has 3 card slots and can use 2 nano SIM cards at the same time, so you can easily switch between work and private use.',
     description3:'【6.52 HD Screen + 3D Color Gradient Design】 With its nearly borderless design, the phone can provide an uninterrupted immersive experience when you play games, watch movies or read e-books. Ulefone Note 10 uses a 3D color gradient design on its back cover. It has a prism-shaped dynamic color gradient on the back, which glows every time it moves. And the matte texture makes it anti-fingerprint. [5500mAh + OTG large-capacity battery] Ulefone Note10P is equipped with 5500mAh high-capacity battery, (10W fast charge) provides a perfect experience, without worrying about the battery life is not long, and you will get rid of the problem of charging. You can enjoy games and videos. It also has OTG reverse charging function.',
-    img: '../assets/img/mobile-models/Ulefon Note 10.webp'
+    img: '../../assets/img/mobile-models/Ulefon Note 10.webp'
   },
   {
     id: 8,
@@ -88,7 +88,7 @@ export const products = [
     description1: 'Redmi 9; The incredible Redmi series offers you the best experience, continuing the legacy of its predecessors. Enhanced imaging system, 13 MP quad camera with AI and 8 MP front selfie camera.',
     description2: 'Redmi 9 features a 6.53 "HD + DotDisplay screen with TÜV Rheinland low blue light certification. MediaTek Helio G80 processor, high-performance 8-core processor. Upgraded 5020mAh (typ) battery, you can enjoy long-lasting power. ',
     description3:'Along with 18W fast charging, you can enjoy your device in no time.',
-    img: '../assets/img/mobile-models/Xiaomi Redmi 9.webp'
+    img: './assets/img/mobile-models/Xiaomi Redmi 9.webp'
   }
 ];
 
